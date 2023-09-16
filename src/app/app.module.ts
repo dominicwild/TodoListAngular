dce038685bb50741562d96e3a0937b94095c0d7c
dominicwild <dominicwild@dominicwild.com>
2023-09-16 15:34:02 +0100

    WIP convert to form controls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { TUI_SANITIZER, TuiAlertModule, TuiDialogModule, TuiRootModule } from "@taiga-ui/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoAppComponent } from './components/todo-app/todo-app.component';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { TuiCheckboxBlockModule } from "@taiga-ui/kit";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoAppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    ReactiveFormsModule,
+    TuiCheckboxBlockModule,
   ],
-  providers: [],
+  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
